453f69e1dac63e3d0c0db9c4b1fc3c2a36fd2f8a
Prathwik <64214685+prathwik0@users.noreply.github.com>
2022-12-24 16:35:23 +0530

    Added test json files

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                             <div className="lg:max-w-lg">
                                 <h1 className="text-3xl font-semibold text-white lg:text-4xl">Best Place To choose your Clothes</h1>
 
-                                <p className="mt-4 text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                                <p className="mt-4 text-gray-400">This is int he file hero.js in the folder components. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
 
                                 <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
                             </div>
