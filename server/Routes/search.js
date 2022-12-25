7503af4a26c161f7cf211c6246fca3c8e40ea111
Srivatsa R Upadhya <srivatsarupadhya@gmail.com>
2022-12-25 21:29:43 +0530

    changed search to give min info

--- server/Routes/search.js
+++ server/Routes/search.js
@@ -1,10 +1,38 @@
 const router = require('express').Router()
+const amazonScraper = require('amazon-buddy');
+const axios = require('axios');
 
-const {getFlipkart} = require('../controllers/flipkart_controller');
-const {getAmazon} = require('../controllers/amazon_controller');
-const {main_search} = require('../controllers/main_search_controller');
+router.get('/:qs', (req, res) => {
+    const query_string = req.params.qs;
+    console.log(query_string);
 
-router.get('/:qs', main_search);
-// router.get('/:qs', getAmazon);
+    const get_amazon = new Promise(async (resolve, reject) => {
+        try {
+            const products = await amazonScraper.products({ keyword : req.params.qs})
+            resolve(products)
+        } catch (err) {
+            reject(console.log(err));
+        }
+    })
+
+    var options_flipkart = {
+        method: 'GET',
+        url: `https://flipkart.dvishal485.workers.dev/search/${query_string}`,
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    };
+
+    Promise.all([get_amazon, axios.request(options_flipkart)])
+        .then(resp => {
+            resp_obj = {
+                amazon: resp[0].result,
+                flipkart: resp[1].data.result
+            }
+            res.status(200).json(resp_obj)
+            console.log("Data sent to client");
+        }).catch(error => {
+            console.log(error);
+        })
+})
+
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const port = 8000;
 
 app.use(cors())
-const searchRoute = require('./Routes/search4')
+const searchRoute = require('./Routes/search')
+const productRoute = require('./Routes/product')
 app.get('/', (req,res)=>{
     res.send("Live");
 });
 
 app.use('/search', searchRoute);
+app.use('/product', productRoute);
 
 app.listen(port, err=>{
     if(err){
